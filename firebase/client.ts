dfe3012d04dc967f7e60591a871af61445301795
mohammad-pakkir-mydeen <126335759+mohammad-pakkir-mydeen@users.noreply.github.com>
2025-05-06 18:55:09 +0530

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDlwfDkIHPvHUhO07wyHJ_s_UTCdd8g9mU",
+  authDomain: "mockmate-20908.firebaseapp.com",
+  projectId: "mockmate-20908",
+  storageBucket: "mockmate-20908.firebasestorage.app",
+  messagingSenderId: "436866693906",
+  appId: "1:436866693906:web:4d1082c4eac954cecfd1cb",
+  measurementId: "G-F5HTFKPHL7",
 };
 
 // Initialize Firebase
